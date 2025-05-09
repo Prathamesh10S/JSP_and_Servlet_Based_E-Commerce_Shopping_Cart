8f07ec8ff17247193a40ba960020a15256c10c60
Prathamesh10S <salunkep341@gmail.com>
2025-05-09 20:22:35 +0530

    "initial commit"

---
+++ src/main/java/shop_cart_Connection/db_con.java
@@ -0,0 +1,21 @@
+package shop_cart_Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class db_con {
+
+	private static Connection connection = null;
+
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		if (connection == null) {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/e_shopping_cart", "root",
+					"Pratham@10");
+			System.out.println("connected");
+
+		}
+		return connection;
+	}
+}
